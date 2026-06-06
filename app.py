c424d15f58489f7c0969b71c0cd7a3dcbbe5ac3a
MOHDIbrahim <ibrahim12hal@gmail.com>
2026-06-06 14:10:39 +0530

    changes some app

--- app.py
+++ app.py
@@ -51,6 +51,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 migrate = Migrate(app, db)
+with app.app_context():
+    db.create_all()
 
 limiter = Limiter(
     key_func=get_remote_address,
